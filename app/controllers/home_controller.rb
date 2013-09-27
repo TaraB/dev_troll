cacb6de5e362cf54511bd4c7bc18b22382bad7fc
Tara Bloom <tara@madebymany.co.uk>
2013-09-27 15:59:52 +0100

    linking to trolls from the homescreen

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,10 @@ def index
  rand_id = rand(Troll.first.id...Troll.last.id)
  rand_record = Troll.first(:conditions => [ "id >= ?", rand_id])
  @randomtroll = rand_record
-  end
+
+ @alldevs = Dev.all
+
+end
+
 
 end
